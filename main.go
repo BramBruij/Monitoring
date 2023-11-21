413d71a76e20b48da1a873b59e2147beb1375a8b
BramBruij <brambruyninckx@live.nl>
2023-11-21 10:37:59 +0100

    Ping website applicatie

--- main.go
+++ main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-ping/ping"
+)
+
+func pingHost(hostname string, ch chan<- string) {
+	pinger, err := ping.NewPinger(hostname)
+	if err != nil {
+		ch <- fmt.Sprintf("Failed to create pinger for %s: %s", hostname, err)
+		return
+	}
+
+	pinger.Count = 3 // Aantal ping-pakketten verzonden
+	pinger.Timeout = time.Second * 2
+
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		ch <- fmt.Sprintf("%s is online", hostname)
+	}
+
+	pinger.OnIdle = func() {
+		ch <- fmt.Sprintf("%s is offline", hostname)
+	}
+
+	err = pinger.Run()
+	if err != nil {
+		ch <- fmt.Sprintf("Failed to ping %s: %s", hostname, err)
+	}
+}
+
+func main() {
+	hostnames := []string{"https://www.fontys.nl/Over-Fontys/Fontys-ICT.htm", "google.com", "nonexistent-host.com"}
+
+	resultChannel := make(chan string)
+
+	for _, hostname := range hostnames {
+		go pingHost(hostname, resultChannel)
+	}
+
+	for i := 0; i < len(hostnames); i++ {
+		result := <-resultChannel
+		fmt.Println(result)
+	}
+
+	close(resultChannel)
+}
