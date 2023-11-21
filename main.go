836a57931db34a6960921f806a9f778511cca1d6
BramBruij <brambruyninckx@live.nl>
2023-11-21 11:10:02 +0100

    Ping

--- main.go
+++ main.go
@@ -2,46 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
-
-	"github.com/go-ping/ping"
 )
 
-func pingHost(hostname string, ch chan<- string) {
-	pinger, err := ping.NewPinger(hostname)
+func pingWebsite(url string, ch chan<- string) {
+	startTime := time.Now()
+
+	resp, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprintf("Failed to create pinger for %s: %s", hostname, err)
+		ch <- fmt.Sprintf("Failed to ping %s: %s", url, err)
 		return
 	}
+	defer resp.Body.Close()
 
-	pinger.Count = 3 // Aantal ping-pakketten verzonden
-	pinger.Timeout = time.Second * 2
-	pinger.Interval = time.Second * 5 // Vergroot de intervaltijd naar 5 seconden
+	elapsed := time.Since(startTime)
 
-	pinger.OnRecv = func(pkt *ping.Packet) {
-		ch <- fmt.Sprintf("%s is online", hostname)
-	}
-
-	pinger.OnIdle = func() {
-		ch <- fmt.Sprintf("%s is offline", hostname)
-	}
-
-	err = pinger.Run()
-	if err != nil {
-		ch <- fmt.Sprintf("Failed to ping %s: %s", hostname, err)
-	}
+	ch <- fmt.Sprintf("%s is online (Response Time: %s)", url, elapsed)
 }
 
 func main() {
-	hostnames := []string{"www.fontys.nl", "google.com", "nonexistent-host.com"}
+	websites := []string{"http://www.google.com", "http://www.example.com", "http://www.nonexistent-website.com"}
 
 	resultChannel := make(chan string)
 
-	for _, hostname := range hostnames {
-		go pingHost(hostname, resultChannel)
+	for _, url := range websites {
+		go pingWebsite(url, resultChannel)
 	}
 
-	for i := 0; i < len(hostnames); i++ {
+	for i := 0; i < len(websites); i++ {
 		result := <-resultChannel
 		fmt.Println(result)
 	}
